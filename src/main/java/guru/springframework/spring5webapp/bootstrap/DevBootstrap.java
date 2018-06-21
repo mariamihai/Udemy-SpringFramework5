12d6be8018185305d971683f9fcfe17f6efa8e14
mariamihai <maria.ic.mihai@gmail.com>
2018-06-21 03:02:21 +0300

    (Section 2, Lecture 19 + 20) - add Publisher entity and repo - modify Book entity - add dummy data for publisher in relation to books

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,13 +15,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
-
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
@@ -27,9 +30,18 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     private void initData() {
+        // Publisher 1
+        Publisher publisher1 = new Publisher("NamePublisher1", "AddressPublisher1");
+        publisherRepository.save(publisher1);
+
+        // Publisher2
+        Publisher publisher2 = new Publisher("NamePublisher2", "AddressPublisher2");
+        publisherRepository.save(publisher2);
+
+
         // Author 1
         Author author1 = new Author("FirstName1", "LastName1");
-        Book book1 = new Book("NameBook1", "12345", "Publisher1");
+        Book book1 = new Book("NameBook1", "12345", publisher1);
         author1.getBooks().add(book1);
         book1.getAuthors().add(author1);
 
@@ -38,7 +50,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         // Author 2
         Author author2 = new Author("FirstName2", "LastName2");
-        Book book2 = new Book("NameBook2", "23456", "Publisher2");
+        Book book2 = new Book("NameBook2", "23456", publisher2);
         author2.getBooks().add(book2);
 
         authorRepository.save(author2);
