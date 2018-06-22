8bb1e2291d17fc07e43e582a3c52b9d6cd89603c
mariamihai <maria.ic.mihai@gmail.com>
2018-06-22 14:13:39 +0300

    (Section 3, Lecture 29) - First commit for Dependency Injection related code

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -1,12 +1,17 @@
 package guru.springframework.didemo;
 
+import guru.springframework.didemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController controller = (MyController) ctx.getBean("myController");
+        controller.hello();
     }
 }
---
+++ src/main/java/guru/springframework/didemo/controllers/MyController.java
@@ -0,0 +1,11 @@
+package guru.springframework.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public void hello() {
+        System.out.println("Hellooo!!!!!");
+    }
+}
