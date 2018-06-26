c3408595d01aef9d09458fcb21ae0fa49c4451c8
mariamihai <maria.ic.mihai@gmail.com>
2018-06-26 15:57:52 +0300

    Spring Profiles - Section 3, Lecture 36 - use @Profile and add multiple beans as @Primary on different profiles - add active profile in application.properties

--- src/main/java/guru/springframework/didemo/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimarySpanishGreetingService.java
@@ -5,13 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("en")
+@Profile("es")
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+public class PrimarySpanishGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello, Primary Greeting Service";
+        return "Servicio de Saludo Primario";
     }
-    //...
 }
