79b4b312a5f1df5d6e023e093b909a0ae8385d8b
mariamihai <maria.ic.mihai@gmail.com>
2018-06-26 17:14:16 +0300

    Spring Profiles - Section 3, Lecture 37 - use default profile

--- src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"en", "default"})
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello, Primary Greeting Service";
+        return "Guten Tag";
     }
 }
