c7a22b8acc7d76099e5bc7cb869b0e90a3bc8591
mariamihai <maria.ic.mihai@gmail.com>
2018-07-05 09:45:10 +0300

    Creating the Spring MVC Controller (Section 4, Lecture 54) - added Autowired annotation to express intent (not specifically needed) - updated RequestMapping annotation

--- src/main/java/com/spring/app/jokes/controllers/JokeController.java
+++ src/main/java/com/spring/app/jokes/controllers/JokeController.java
@@ -1,6 +1,7 @@
 package com.spring.app.jokes.controllers;
 
 import com.spring.app.jokes.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class JokeController {
     private JokeService jokeService;
 
+    @Autowired
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping("/")
+    @RequestMapping({"/", ""})
     public String getJoke(Model model) {
         model.addAttribute("joke", jokeService.getJoke());
         return "chucknorris";
